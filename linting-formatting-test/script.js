c66a93deb4a6aae5f176aff6f1771090509e43dd
tritonro <106782621+tritonro@users.noreply.github.com>
2025-05-11 22:03:27 -0700

    pushing code without semicolons to test linter

--- linting-formatting-test/script.js
+++ linting-formatting-test/script.js
@@ -1,4 +1,15 @@
 function clickFunction(){alert('Button Clicked!');}var x=5;var y=10;var z=x+y;console.log(z);
 
 
+//code with no semicolons adapted from https://javascript.info/coding-style
+function pow(x, n) {
+  let result = 1
+  //              <--
+  for (let i = 0; i < n; i++) {
+result *= x
+  }
+  //              <--
+     return result;
+}
 
+pow (3, 2)
