cbd83a061a95e0ac3e50983f62e46be40c4fc63a
tritonro <106782621+tritonro@users.noreply.github.com>
2025-05-30 00:14:39 -0700

    Modified calculateStreak() to match card object data layout in ADR

--- out/scripts/CRUD.js
+++ out/scripts/CRUD.js
@@ -1,14 +1,15 @@
+const DAYINMS = 86400000;
+
 export const localStorageAdapter = {
-    get: (key) => localStorage.getItem(key),
-    set: (key, value) => localStorage.setItem(key, value ),
-    del: (key) => localStorage.removeItem(key), 
-    len: ()=> localStorage.length,
-    keys: ()=> Object.keys(localStorage), 
+  get: (key) => localStorage.getItem(key),
+  set: (key, value) => localStorage.setItem(key, value),
+  del: (key) => localStorage.removeItem(key),
+  len: () => localStorage.length,
+  keys: () => Object.keys(localStorage),
 };
 
-
-function typeErrorTemplate (stringParam, type){
-  return `${type} is the incorrect type for ${stringParam})`
+function typeErrorTemplate(stringParam, type) {
+  return `${type} is the incorrect type for ${stringParam})`;
 }
 
 /**
@@ -16,52 +17,60 @@ function typeErrorTemplate (stringParam, type){
  * @param habitDescription string description of the habit
  * @param habitFrequency integer number of days representing a frequency (ex. Daily = 1, Weekly = 7)
  * @param startDateTime date string representing the first occurrence of habit
- * @param adapter defaults to localStorageAdapter, allows means to use other storage methods 
+ * @param adapter defaults to localStorageAdapter, allows means to use other storage methods
  * @type {(habitName : String, habitDescription : String, startDateTime : String, adapter : Object) => String}
- * habitStreak & logs will also be fields of this object, but are not parameters because they are initialized to default values 
+ * habitStreak & logs will also be fields of this object, but are not parameters because they are initialized to default values
  * @return habitID, a unique ID string
  */
-export function createHabit(habitName, habitDescription, habitFrequency, startDateTime,  adapter = localStorageAdapter){
-   if (typeof habitName != "string"){
-     throw new Error (typeErrorTemplate(habitName, typeof(habitName)));
-   }
-
-  if (typeof habitDescription != "string"){
-    throw new Error (typeErrorTemplate(habitDescription, typeof(habitDescription)));
+export function createHabit(
+  habitName,
+  habitDescription,
+  habitFrequency,
+  startDateTime,
+  adapter = localStorageAdapter,
+) {
+  if (typeof habitName != 'string') {
+    throw new Error(typeErrorTemplate(habitName, typeof habitName));
   }
-   /*
+
+  if (typeof habitDescription != 'string') {
+    throw new Error(
+      typeErrorTemplate(habitDescription, typeof habitDescription),
+    );
+  }
+  /*
    Choosing to represent frequencies as integer hour values to compromise between
    * readability & compatibility with JS Date() objects, which operate in ms
    * ex: Daily = 24, Weekly = 168, 30 days = 720
    * make names in local storage match parameters
    * */
-   if(typeof habitFrequency != "number"){
-     throw new Error (typeErrorTemplate(habitFrequency, typeof(habitFrequency)));
-   }
-   /*check that startDateTime is a valid dateString (could be in standard or UTC string format, just need to be acceptable to Date.parse())
+  if (typeof habitFrequency != 'number') {
+    throw new Error(typeErrorTemplate(habitFrequency, typeof habitFrequency));
+  }
+  /*check that startDateTime is a valid dateString (could be in standard or UTC string format, just need to be acceptable to Date.parse())
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse
     Requires regex to do properly: https://stackoverflow.com/questions/7445328/check-if-a-string-is-a-date-value
     */
 
-    let habit = {
-        habitName: habitName,
-        habitDescription: habitDescription,
-        habitFrequency: habitFrequency,
-        startDateTime: startDateTime,
-        habitStreak: 0,
-        logs: []
-    };
+  let habit = {
+    habitName: habitName,
+    habitDescription: habitDescription,
+    habitFrequency: habitFrequency,
+    startDateTime: startDateTime,
+    habitStreak: 0,
+    logs: [],
+  };
 
-    let habitID = "id" + self.crypto.randomUUID();
-    adapter.set(habitID, JSON.stringify(habit));
-    return habitID;
+  let habitID = 'id' + self.crypto.randomUUID();
+  adapter.set(habitID, JSON.stringify(habit));
+  return habitID;
 }
 
 /**
  * @param {String} habitID the string id of the habit being read
  * @param {Object} adapter an object representing our database (currenly only use localStorageAdapter)
  */
-export function readHabit(habitID, adapter = localStorageAdapter){
+export function readHabit(habitID, adapter = localStorageAdapter) {
   return adapter.get(habitID);
 }
 
@@ -71,47 +80,47 @@ export function readHabit(habitID, adapter = localStorageAdapter){
  * @param {list} newValues list of new values for fields (passed in the same order).
  * New values should follow the type requirements of createhabit()
  */
-export function updateHabit(habitID, fields, newValues){
+export function updateHabit(habitID, fields, newValues) {
   //check that fields and newValues are the same length
-  if(fields.length != newValues.length){
-    throw new Error ('fields and newValues must have the same length');
+  if (fields.length != newValues.length) {
+    throw new Error('fields and newValues must have the same length');
   }
-  const stringFields = new Set ("habitName", "habitDescription" );
-  const numberFields = new Set ("habitFrequency", "habitStreak");
+  const stringFields = new Set('habitName', 'habitDescription');
+  const numberFields = new Set('habitFrequency', 'habitStreak');
   let habitToUpdate = adapter.get(habitID);
   habitToUpdate = JSON.parse(habitToUpdate);
-  for (let i = 0; i < fields.length; i++){
-      if (!Object.hasOwn(habitToUpdate, fields[i])) {
-        throw new Error (`${fields[i]} is not a valid habit field`);
-      }
-      if (fields[i] in stringFields){
-        habitToUpdate.fields[i] = newValues[i];
-      }
-      else if (fields[i] in numberFields){
-        if (!newValues[i]){ //will reject null, false, 0 but not "0", dependent on all values being parsed as strings
-          throw new Error (`${fields[i]} is not a valid number field`);
-        }
-        habitToUpdate.fields[i] =  newValues[i];
-      }
-      //checking for date strings, rejecting everything else (requires regex)
-      else{
-
-      }
-      habitToUpdate.fields[i] = newValues[i]; 
+  for (let i = 0; i < fields.length; i++) {
+    if (!Object.hasOwn(habitToUpdate, fields[i])) {
+      throw new Error(`${fields[i]} is not a valid habit field`);
     }
+    if (fields[i] in stringFields) {
+      habitToUpdate.fields[i] = newValues[i];
+    } else if (fields[i] in numberFields) {
+      if (!newValues[i]) {
+        //will reject null, false, 0 but not "0", dependent on all values being parsed as strings
+        throw new Error(`${fields[i]} is not a valid number field`);
+      }
+      habitToUpdate.fields[i] = newValues[i];
+    }
+    //checking for date strings, rejecting everything else (requires regex)
+    else {
+    }
+    habitToUpdate.fields[i] = newValues[i];
+  }
 }
 
 /**
- * 
+ *
  * @param {String} cardID, the string ID of the habit being deleted
- * @param {Object} adapter defaults to localStorageAdapter, allows means to use other storage methods 
+ * @param {Object} adapter defaults to localStorageAdapter, allows us to pass in other storage methods
  */
 export function deleteHabit(cardID, adapter = localStorageAdapter) {
-  return adapter.del(id); 
-} 
+  return adapter.del(id);
+}
 
 /**
- * @return a list of habit objects 
+ * @param adapter {Object} defaults to localStorageAdapter, allows us to pass in other storage methods
+ * @return a list of habit objects or null if none are found
  */
 export function getAllHabits(adapter = localStorageAdapter){
   let habits = []; 
@@ -184,41 +193,39 @@ function deleteHabit(id) {
   saveHabits(habits);
 }
 
+/**
+ *
+ * @param {Object} habit a JSON object representing a habit(not a habit string !) 
+ * @returns
+ */
 function calculateStreak(habit) {
-  let logs = habit.logs.sort();
+  let logs = habit.logs;
+  
   if (logs.length < 2) {
     return logs.length;
   }
+  let ms = 0;
+  let msLogs = []; 
+
+  //Date.parse() is very format permissive, this needs to be tested thoroughly
+  logs.forEach((element) => {
+    ms = Date.parse(element);
+    if (ms == NaN){
+      throw new Error(typeErrorTemplate(logs,  typeof ms));
+    }
+    msLogs.push(ms); 
+  });
+
+  msLogs = msLogs.sort();
   let streak = 0;
-  if (habit.frequency == 'daily') {
-    let exp = Math.floor(Date.now() / DAYINMS);
-    for (let i = -2; i >= -1 * logs.length; i--) {
-      if (Math.floor(logs[i]) != exp) {
-        return streak;
-      } else {
-        streak++;
-        exp--;
-      }
-    }
-  } else if (habit.frequency == 'weekly') {
-    let exp = Math.floor(Date.now() / DAYINMS);
-    for (let i = -2; i >= -1 * logs.length; i--) {
-      if (Math.floor(logs[i]) > exp) {
-        return streak;
-      } else {
-        streak++;
-        exp -= 7;
-      }
-    }
-  } else {
-    let exp = Math.floor(Date.now() / DAYINMS);
-    for (let i = -2; i >= -1 * logs.length; i--) {
-      if (Math.floor(logs[i] / DAYINMS) > exp) {
-        return streak;
-      } else {
-        streak++;
-        exp -= 30;
-      }
+  let exp = Math.floor(Date.now() / DAYINMS);
+
+  for (let i = -2; i >= -1 * logs.length; i--) {
+    if (Math.floor(logs[i]) != exp) {
+      return streak;
+    } else {
+      streak++;
+      exp -= habit.frequency;
     }
   }
 }
