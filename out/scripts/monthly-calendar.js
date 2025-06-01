8e9bad60cd0cf118a2d9fbe49d9b5dfb592e4b5f
t-m1 <103397078+t-m1@users.noreply.github.com>
2025-05-31 19:58:36 -0700

    added daily icon

--- out/scripts/monthly-calendar.js
+++ out/scripts/monthly-calendar.js
@@ -2,9 +2,13 @@
 const home_select = document.getElementById('home-selection');
 const calendar_select = document.getElementById('calendar-selection');
 const settings_select = document.getElementById('settings-selection');
+const daily_calendar_select = document.getElementById('daily-calendar-selection');
 
 home_select.addEventListener('click', () => {
   window.location.href = 'home-page.html';
+});
+daily_calendar_select.addEventListener('click', () => {
+  window.location.href = 'daily-calendar.html';
 });
 calendar_select.addEventListener('click', () => {
   window.location.href = 'monthly-calendar.html';
