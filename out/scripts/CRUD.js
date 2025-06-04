71db5e180f2cd7bd1fefe93339666ba7b418e8df
Charan Battula <charanb2005@gmail.com>
2025-06-03 17:19:27 -0700

    formatting

--- out/scripts/CRUD.js
+++ out/scripts/CRUD.js
@@ -1,5 +1,6 @@
 const DAYINMS = 86400000;
-const uuidRegex = /^id[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+const uuidRegex =
+  /^id[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 //export { getHabitsForToday, createHabit, deleteHabit } from './CRUD.js';
 
 export const localStorageAdapter = {
