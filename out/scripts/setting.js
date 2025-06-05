52bd84bb71691d2ceb1da2e85cdbf3f6584de9df
trishangvyen <trn014@ucsd.edu>
2025-06-04 20:06:59 -0700

    Update setting.js directory

--- out/scripts/setting.js
+++ out/scripts/setting.js
@@ -1,6 +1,6 @@
 const themeSelect = document.getElementById('theme-select');
 const body = document.body; // Add this line!
-import { localStorageAdapter } from '/out/scripts/crud.js';
+import { localStorageAdapter } from './out/scripts/crud.js';
 // On change, save the selection
 themeSelect.addEventListener('change', (e) => {
   const selectedTheme = e.target.value;
